755795136c2c0052450289b49a2005b5240420fa
Ymoka <contact@lucasengelmann.fr>
2024-12-23 23:06:54 +0100

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Variable : camelCase
+            int number = 1;
+            //Constant : PascalCase
+            const float Pi = 3.14f;
+
+            Console.WriteLine(number);
+            Console.WriteLine(Pi);
+            //String template filled with variables
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            
+            //Basic test of overflowing a variable
+            try
+            {
+                checked
+                {
+                    byte maxBite = byte.MaxValue;
+                    maxBite++;
+                    Console.WriteLine(maxBite);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception volontaire : " + e.Message);
+                // Removed throw; to avoit e
+            }
+            
+        }
+
+    }
+}
